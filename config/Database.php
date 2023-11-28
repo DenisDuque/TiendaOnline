e56457eb43d5bc672a8b47f991129c5342d7c5dc
SergiAlonsoAlomar <sergi.alonso@inslapineda.cat>
2023-11-28 10:46:27 +0100

    config.php cambiado

--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,18 @@
 <?php
+try {
+    // Configuración de la base de datos
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "TiendaOnline";
+    
+    // Crear conexión
+    $connectPdo = new PDO('pgsql:host='.$servername.';dbname='.$dbname, $username, $password);
+    $connectPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Establecer el conjunto de caracteres a UTF-8
+    $connectPdo->exec("SET NAMES 'utf8'");
 
-// Configuración de la base de datos
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "TiendaOnline";
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+} catch (PDOException $e) {
+    echo "Error de PDO: " . $e->getMessage();
 }
-
-// Establecer el conjunto de caracteres a UTF-8
-$conn->set_charset("utf8");
 ?>
