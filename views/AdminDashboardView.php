465a93990284c4a1796f9a6908cce9fd96e3c37d
SergiAlonsoAlomar <sergi.alonso@inslapineda.cat>
2023-11-28 16:48:16 +0100

    login scss hecho

--- views/AdminDashboardView.php
+++ views/AdminDashboardView.php
@@ -8,30 +8,30 @@
     <body>
         <h1>Urban Store</h1>
         <div>
-            <img src="assets/images/utils/signout.png" alt="Sign out">
+            <img src="views/assets/images/utils/signout.png" alt="Sign out">
         </div>
         <h2>Administrator Panel</h2>
         <div class='panel' id='categories'>
             <div class='image'>
-                <img src="assets/images/utils/categories.png" alt="Categories">
+                <img src="views/assets/images/utils/categories.png" alt="Categories">
             </div>
             <p>Categories</p>
         </div>
         <div class='panel' id='products'>
             <div class='image'>
-                <img src="assets/images/utils/products.png" alt="Products">
+                <img src="views/assets/images/utils/products.png" alt="Products">
             </div>
             <p>Products</p>
         </div>
         <div class='panel' id='customers'>
             <div class='image'>
-                <img src="assets/images/utils/customers.png" alt="Customers">
+                <img src="views/assets/images/utils/customers.png" alt="Customers">
             </div>
             <p>Customers</p>
         </div>
         <div class='panel' id='orders'>
             <div class='image'>
-                <img src="assets/images/utils/orders.png" alt="Orders">
+                <img src="views/assets/images/utils/orders.png" alt="Orders">
             </div>
             <p>Orders</p>
         </div>
