64e86fe516ec7972049742ca3cda150d7044067b
Joel <joel.gurrera@inslapineda.cat>
2023-11-28 11:52:03 +0100

    modelo con getters y setters

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,14 +1,69 @@
 <?php
-require_once 'config/Database.php';
 class UserModel {
+    private $email;
+    private $phone;
+    private $name;
+    private $surname;
+    private $address;
+    private $rol;
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setPhone($phone) {
+        $this->phone = $phone;
+    }
+
+    public function getPhone() {
+        return $this->phone;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setSurname($surname) {
+        $this->surname = $surname;
+    }
+
+    public function getSurname() {
+        return $this->surname;
+    }
+
+    public function setAddress($address) {
+        $this->address = $address;
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function setRol($rol) {
+        $this->rol = $rol;
+    }
+
+    public function getRol() {
+        return $this->rol;
+    }
+
     public function authenticate($userEmail, $password) {
         // Lógica de autenticación (consulta a la base de datos)
         // Devuelve true si la autenticación es exitosa, false de lo contrario
     }
 
     public function register($userName, $userEmail, $userPassword) {
-        // Lógica de registro (consulta a la base de datos)
-        // Devuelve true si el registro es exitoso, false de lo contrario
+        $validRegister = false;
+        $selectUser = "SELECT * FROM users WHERE email LIKE $userEmail";
+
     }
 }
 ?>
