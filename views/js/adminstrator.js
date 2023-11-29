5fb47e3c0dff14fecc9137c41031ff71f33e95c9
Joel <joel.gurrera@inslapineda.cat>
2023-11-29 09:08:01 +0100

    commit

--- views/js/adminstrator.js
+++ views/js/adminstrator.js
@@ -1,4 +1,5 @@
 var panels = document.getElementsByClassName("panel");
+var editButtons = document.getElementsByClassName("editButtons");
 
 for(var i = 0; i < panels.length; i++){
     panels[i].addEventListener("click", function(e){
@@ -6,5 +7,11 @@ for(var i = 0; i < panels.length; i++){
         var meta = document.createElement("meta");
         meta.httpEquiv = "refresh";
         meta.content = "0:index.php?page=admnistrator&panel="+id;
+    })
+}
+
+for(var i = 0; i < editButtons.length; i++){
+    editButtons[i].addEventListener("click", function(e){
+        let div = document.getElementById("panelContainer");
     })
 }
