27f80400591ea0fb979ec7571990ac189d43f346
DenisDuque <d.duque.dev@gmail.com>
2023-11-30 15:10:48 +0100

    Añadido control de carga en Admin.js

--- views/js/Administrator.js
+++ views/js/Administrator.js
@@ -1,16 +1,18 @@
-var panels = document.getElementsByClassName("panel");
-var editButtons = document.getElementsByClassName("editButtons");
+document.addEventListener('DOMContentLoaded', function() {
+    var panels = document.getElementsByClassName("panel");
+    var editButtons = document.getElementsByClassName("editButtons");
 
-for (let i = 0; i < panels.length; i++) {
-    panels[i].addEventListener("click", function (e) {
-        let id = panels[i].id;
-        window.location.href = "index.php?page=administrator&panel=" + id;
-    });
-}
+    for (let i = 0; i < panels.length; i++) {
+        panels[i].addEventListener("click", function (e) {
+            let id = panels[i].id;
+            window.location.href = "index.php?page=administrator&panel=" + id;
+        });
+    }
 
 
-for(var i = 0; i < editButtons.length; i++){
-    editButtons[i].addEventListener("click", function(e){
-        let div = document.getElementById("panelContainer");
-    })
-}
+    for(var i = 0; i < editButtons.length; i++){
+        editButtons[i].addEventListener("click", function(e){
+            let div = document.getElementById("panelContainer");
+        })
+    }
+});
