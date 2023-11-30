e7b70a3e8dc0eebb2e2f6fca35f6636632fcf5f0
SergiAlonsoAlomar <sergi.alonso@inslapineda.cat>
2023-11-30 12:54:37 +0100

    es para cambiar la bbdd

--- config/Database.php
+++ config/Database.php
@@ -1,28 +1,32 @@
 <?php
 
 class Database{
-   
-    public function connect(){
+	protected static $conn;
 
-        try {
-            // Configuración de la base de datos
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "urbanstore";
-            
-            // Crear conexión
-            $connectPdo = new PDO('pgsql:host='.$servername.';dbname='.$dbname, $username, $password);
-            $connectPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // Establecer el conjunto de caracteres a UTF-8
-            $connectPdo->exec("SET NAMES 'utf8'");
-            return $connectPdo;
-        
-        } catch (PDOException $e) {
-            echo "Error de PDO: " . $e->getMessage();
-        }
+	public function __construct() {
+    	self::$conn = self::connect();
+	}
+	public static function connect() {
+
+    	try {
+        	// Configuración de la base de datos
+        	$servername = "localhost";
+        	$username = "postgres";
+        	$password = "admin";
+        	$dbname = "urbanstore";
+       	 
+        	// Crear conexión
+        	$connectPdo = new PDO('pgsql:host='.$servername.';dbname='.$dbname, $username, $password);
+        	$connectPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        	// Establecer el conjunto de caracteres a UTF-8
+        	$connectPdo->exec("SET NAMES 'utf8'");
+        	return $connectPdo;
+   	 
+    	} catch (PDOException $e) {
+        	echo "Error de PDO: " . $e->getMessage();
+    	}
     
-    }
+	}
     
 }
 
