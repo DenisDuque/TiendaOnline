24aa797853c8ce55bdfa4be91368e1b0b0ff89ea
AlexAlcalaGarcia <92880596+AlexAlcalaGarcia@users.noreply.github.com>
2023-12-04 14:02:17 +0100

    funcion que muestra ordenes

--- controllers/AdminOrdersController.php
+++ controllers/AdminOrdersController.php
@@ -12,14 +12,52 @@ class AdminOrdersController {
         $Orders = OrdersModel::getOrders();
         foreach($Orders as $order) {
             $userInfo = UserModel::getSpecifiedUser($order->getUser());
-            $productsInfo = OrdersModel::getMyProducts($order->getId());
-            echo "
-                <div>
-
-                </div>
-            ";
+            $productsID = OrdersModel::getMyProducts($order->getId());
+            $showProducts = "";
+            if (count($productsID) <= 3) {
+                $showProducts = implode(", ", $productsID);
+            } else {
+                $firstProducts = array_slice($productsID, 0, 3);
+                $showProducts = implode(", ", $firstProducts) + "...";
+            }
+            if($userInfo->getImage() != null) {
+                echo "
+                    <div>
+                        <div>
+                            <img src='../assets/images/utils/customer.png' alt='Customer's Image'>
+                        </div>
+                        <div>
+                            <div>". $userInfo->getName() ." ". $userInfo->getSurname() ."</div>
+                            <div>Email: ". $userInfo->getEmail() ."</div>
+                            <div>Products: ". $showProducts ."</div>
+                            <div><div>Total Amount: ". $order->getPrice() ."</div><div>Status: ". $order->getStatus() ."</div></div>
+                        </div>
+                        <div>
+                            <div><button type='submit' id='". $order->getId() ." bill'><img src='../assets/images/utils/factura.png' alt='Factura'></button></div>
+                            <div><button type='submit' id='". $order->getId() ." edit'><img src='../assets/images/utils/edit.png' alt='Edit'></button></div>
+                        </div>
+                    </div>
+                ";
+            } else {
+                echo "
+                    <div>
+                        <div>
+                            <img src='../assets/images/users/". $userInfo->getImage() ."' alt='Customer's Image'>
+                        </div>
+                        <div>
+                            <div>". $userInfo->getName() ." ". $userInfo->getSurname() ."</div>
+                            <div>Email: ". $userInfo->getEmail() ."</div>
+                            <div>Products: ". $showProducts ."</div>
+                            <div><div>Total Amount: ". $order->getPrice() ."</div><div>Status: ". $order->getStatus() ."</div></div>
+                        </div>
+                        <div>
+                            <div><button type='submit' id='". $order->getId() ." bill'><img src='../assets/images/utils/factura.png' alt='Factura'></button></div>
+                            <div><button type='submit' id='". $order->getId() ." edit'><img src='../assets/images/utils/edit.png' alt='Edit'></button></div>
+                        </div>
+                    </div>
+                ";
+            }
         }
-        //$userInfo = UserModel::getSpecifiedUser($Orders['']);
     }
 }
 
