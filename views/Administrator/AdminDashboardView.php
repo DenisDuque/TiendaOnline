d84976b07767aa448ccd483a4937416ed22e9714
DenisDuque <d.duque.dev@gmail.com>
2023-12-04 13:10:37 +0100

    CSS Actualizado

--- views/Administrator/AdminDashboardView.php
+++ views/Administrator/AdminDashboardView.php
@@ -8,40 +8,45 @@
         <title>Dashboard</title>
     </head>
     <body>
-        <div>
+        <header>
             <h1>Urban Store</h1>
             <img src="views/assets/images/utils/signout.png" alt="Sign out">
-        </div>
-        <h2>Administrator Panel</h2>
-        <div class="panels">
-            <div class='panel' id='categories'>
-                <div class='image'>
-                    <img src="views/assets/images/utils/categories.png" alt="Categories">
+        </header>
+        <div id="container">
+            <div id="leftPanel">
+                <div class="panels">
+                    <div class='panel' id='categories'>
+                        <div class='image'>
+                            <img src="views/assets/images/utils/categories.png" alt="Categories">
+                        </div>
+                        <p>Categories</p>
+                    </div>
+                    <div class='panel' id='products'>
+                        <div class='image'>
+                            <img src="views/assets/images/utils/products.png" alt="Products">
+                        </div>
+                        <p>Products</p>
+                    </div>
+                    <div class='panel' id='customers'>
+                        <div class='image'>
+                            <img src="views/assets/images/utils/customers.png" alt="Customers">
+                        </div>
+                        <p>Customers</p>
+                    </div>
+                    <div class='panel' id='orders'>
+                        <div class='image'>
+                            <img src="views/assets/images/utils/orders.png" alt="Orders">
+                        </div>
+                        <p>Orders</p>
+                    </div>
                 </div>
-                <p>Categories</p>
             </div>
-            <div class='panel' id='products'>
-                <div class='image'>
-                    <img src="views/assets/images/utils/products.png" alt="Products">
+            <div id="rightPanel">
+                <h2>Administrator Panel</h2>
+                <div id="listContainer">
+                    <?php AdminDashboardController::bestSellers(); ?>
                 </div>
-                <p>Products</p>
             </div>
-            <div class='panel' id='customers'>
-                <div class='image'>
-                    <img src="views/assets/images/utils/customers.png" alt="Customers">
-                </div>
-                <p>Customers</p>
-            </div>
-            <div class='panel' id='orders'>
-                <div class='image'>
-                    <img src="views/assets/images/utils/orders.png" alt="Orders">
-                </div>
-                <p>Orders</p>
-            </div>
-        </div>
-        <h3>Best Sellers</h3>
-        <div>
-            <?php AdminDashboardController::bestSellers(); ?>
         </div>
     </body>
 </html>
