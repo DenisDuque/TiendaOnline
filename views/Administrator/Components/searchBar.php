f85f09c10b4df6330550596f1b3bd285248dc46d
DenisDuque <d.duque.dev@gmail.com>
2023-12-04 14:21:42 +0100

    CSS barra busqueda parcial

--- views/Administrator/Components/searchBar.php
+++ views/Administrator/Components/searchBar.php
@@ -3,4 +3,9 @@
         echo '<input id="search" type="text" name="search" value="'.$value.'" placeholder="Search">';
     ?>
     <div id="searchBtn"><img src="views/assets/images/utils/search.png" alt="Search"></a></div>
+    <?php
+        if(($_GET['panel'] == 'categories') OR ($_GET['panel'] == 'products')) {
+            echo '<div id="addBtn"><img src="views/assets/images/utils/add.png" alt="Add"></a></div>';
+        }
+    ?>
 </div>
