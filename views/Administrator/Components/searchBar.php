2e40c1c92adb822ce100168081a185dce8ccf5d7
DenisDuque <d.duque.dev@gmail.com>
2023-12-04 16:59:23 +0100

    NavBar acabada

--- views/Administrator/Components/searchBar.php
+++ views/Administrator/Components/searchBar.php
@@ -1,8 +1,10 @@
-<div id='searcher'>
-    <?php $value = isset($_GET['search']) ? $_GET['search'] : ''; 
-        echo '<input id="search" type="text" name="search" value="'.$value.'" placeholder="Search">';
-    ?>
-    <div id="searchBtn"><img src="views/assets/images/utils/search.png" alt="Search"></a></div>
+<div id='navBar'>
+    <div id='searchBar'>
+        <?php $value = isset($_GET['search']) ? $_GET['search'] : ''; 
+            echo '<input id="search" type="text" name="search" value="'.$value.'" placeholder="Search">';
+        ?>
+        <div id="searchBtn"><img src="views/assets/images/utils/search.png" alt="Search"></a></div>
+    </div>
     <?php
         if(($_GET['panel'] == 'categories') OR ($_GET['panel'] == 'products')) {
             echo '<div id="addBtn"><img src="views/assets/images/utils/add.png" alt="Add"></a></div>';
