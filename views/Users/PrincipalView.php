a33eb596ad3525236ddc017d4f0f977e9f9533ab
AlexAlcalaGarcia <92880596+AlexAlcalaGarcia@users.noreply.github.com>
2023-12-15 08:27:45 +0100

    avance pagina principal

---
+++ views/Users/PrincipalView.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Urban Store</title>
+</head>
+<body>
+    <header>
+        <!--Logo Image-->
+        <img src="../assets/images/utils/logo.png" alt="Urban store logo">
+        <!--Title-->
+        <h1>Urban store</h1>
+        <!--Options-->
+        <div id="buttons">
+            <img src="../assets/images/utils/home.png" alt="Home">
+            <img src="../assets/images/utils/search.png" alt="Search">
+            <img src="../assets/images/utils/outstanding.png" alt="Outstanding">
+            <img src="../assets/images/utils/cart.png" alt="Cart">
+            <img src="../assets/images/utils/bell.png" alt="Notifications">
+            <img src="../assets/images/utils/customers.png" alt="Profile">
+        </div>
+    </header>
+    <!--Image slider-->
+    <div>
+        <!--Slider code-->
+    </div>
+    <!--Categories options-->
+    <div>
+        <div>
+            <h2>Categories</h2>
+            <a src="#">Search</a>
+        </div>
+        <div>
+            <!--Categories list code-->
+        </div>
+    </div>
+    <!--All products-->
+    <div>
+        <!--Products list-->
+    </div>
+</body>
+</html>
