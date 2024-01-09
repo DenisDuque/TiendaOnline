6f814b1962108bbc18cb8e5593a43d56d9b60e79
Joel <joel.gurrera@inslapineda.cat>
2024-01-09 09:41:04 +0100

    cambio de clases en tiempo real en pagina de producto

--- views/js/productPage.js
+++ views/js/productPage.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     function changeImage(image){
-        let mainImage = document.getElementById("main");
-        mainImage.src = image;
+        let mainImage = document.getElementById("mainImage");
+        mainImage.children[0].src = image;
     }
     var images = document.getElementsByClassName("image");
 
     for(let i = 0; i<images.length; i++){
-        images[i].addEventListener("click", function(){
-            changeImage(images[i].children[0].src);
-            if(images[i].className == "image"){
-                images[i].className = "clicked";
-            }else{
-                images[i].className = "image";
-
-            }
-        })
+        if(images[i].id != "mainImage"){
+            images[i].addEventListener("click", function(){
+                changeImage(images[i].children[0].src);
+                images[i].classList.add("clicked");
+                for(let x = 0; x<images.length; x++){
+                    if(x!=i){
+                        images[x].classList.remove("clicked");
+                    }
+                }
+            })
+        }
     }
 
 });
