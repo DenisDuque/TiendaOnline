785bd010e4792da76665c93d7de66ff134861940
Joel <joel.gurrera@inslapineda.cat>
2024-01-24 09:58:39 +0100

    btns product page en formularios para usar el post

--- views/General/productPage.php
+++ views/General/productPage.php
@@ -12,6 +12,7 @@
     <?php         
         require_once("views\General\Components\headerHome.html");
     ?>
+    
     <main id="productPage">
     <?php
         echo "<div id='main'>";
@@ -66,27 +67,38 @@
                         echo "<h4>".$product["description"]."</h4>";
                     echo "</div>";
                     echo "<div id='buttons'>";
+                        //wishlist btn
                         if(!isset($_SESSION["email"])){
-                            echo "<a id='wishlist' href='index.php?page=user&action=default&code=".$product["code"]."&function=wishlist'>";
+                            echo "<form action='index.php?page=user&action=default&origin=".$_GET["code"]."' method=post>";
                         }else{
-                            echo "<a id='wishlist' href='index.php?page=product&action=showProduct&code=".$product["code"]."&function=wishlist'>";
-
+                            echo "<form action='index.php?page=product&action=default&origin=".$_GET["code"]."' method=post>";
                         }
-                            echo "<p>WishList</p>";
-                                if($product["inWishList"]){
-                                    echo "<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' class='inWishList'>";
-                                }else{
-                                    echo "<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>";
-                                }
-                                    echo "<path d='M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z'/>";
-                                echo "</svg>";
-                            echo "</a>";
-                        echo "<a id='cart' href='index.php?page=user&action=default&code=".$product["code"]."&function=cart'>Add to cart</a>";
+                                echo "<input type='hidden' name='function' value='wishList'>";
+                                echo "<button type='submit' id='wishlist'>";
+                                    echo "<p>wishlist</p>";
+                                    if($product["inWishList"]){
+                                        echo "<img src='views/assets/images/utils/redHeart.png' alt='heart'>";
+                                    }else{
+                                        echo "<img src='views/assets/images/utils/emptyHeart.png' alt='heart'>";
+                                    }
+                                echo "</button>";
+                            echo "</form>";
+                        
+                        //add to cart btn
+                        if(!isset($_SESSION["email"])){
+                            echo "<form action='index.php?page=user&action=default&origin=".$_GET["code"]."' method=post>";
+                        }else{
+                            echo "<form action='index.php?page=product&action=default&origin=".$_GET["code"]."' method=post>";
+                        }
+                                echo "<input type='hidden' name='function' value='cart'>";
+                                echo "<button type='submit' id='wishlist'>";
+                                    echo "<p>Add to cart</p>";
+                                echo "</button>";
+                            echo "</form>";
                     echo "</div>";
                 echo "</div>";
             echo "</div>";
         echo "</div>";
-        //aqui las tallas que es una movida eso la vd
     ?>
     </main>
 </body>
