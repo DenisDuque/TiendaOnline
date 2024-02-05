c431bbfa21d01028bbdfded55a4a1eac0830b41a
Joel <joel.gurrera@inslapineda.cat>
2024-02-05 08:44:22 +0100

    perfil de usuario empezado

--- views/Users/userProfile.php
+++ views/Users/userProfile.php
@@ -6,9 +6,29 @@
     <title>My profile</title>
 </head>
 <body>
+    <main>
     <?php
-        
+       echo "<div>";
+            echo "<div class='titles'>";
+                echo "<h1>My Orders</h1>";
+                echo "<h1>".count($orders)." items</h1>";
+            echo "</div>";
+            echo "<h2>DATE</h2>";
+            echo "<h2>ORDER DETAILS</h2>";
+            echo "<h2>AMOUNT</h2>";
+            echo "<h2>STATUS</h2>";
+            echo "<h2>ACTIONS</h2>";
+            echo "<div id='orders'>";
+            foreach($orders as $order){
+                echo "<div class='order'>";
+                    echo "<p>".$order["purchasedate"]."</p>";
+                echo "</div>";
+            }
+            echo "</div>";
+
+       echo "</div>"; 
     
-    ?> 
+    ?>
+    </main>
 </body>
 </html>
